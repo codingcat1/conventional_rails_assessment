1792ec804afdaa6fdf536b87c0a84476b1350d5b
Catherine Tran <tranc489@gmail.com>
2014-09-17 16:31:15 -0700

    create users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917175636) do
+ActiveRecord::Schema.define(version: 20140917223511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20140917175636) do
 
   create_table "sessions", force: true do |t|
     t.time     "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
