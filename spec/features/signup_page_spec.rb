634bb993425a3b1b44cac4fde5191388c0cd37a2
Catherine Tran <tranc489@gmail.com>
2014-09-18 17:11:52 -0700

    create 'signing in a new user' spec.  WIP.

---
+++ spec/features/signup_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+# require 'utilities'
+
+# describe "signing up a new user" do
+#   it "allows a currently signed-in user to create a new user" do
+#     user = User.create(:username => 'cat', :password => "123", :password_confirmation => "123")
+#     sign_in(user)
+#     expect(page).to have_content "Sanctuaries"
+#     new_user = User.create(:username => 'bob', :password => "123", :password_confirmation => "123")
+#     sign_up(new_user)
+#   end
+# end
+
+
--- spec/utilities.rb
+++ spec/utilities.rb
@@ -1,13 +1,22 @@
 def sign_in(user)
   visit "/login"
-  fill_in 'name', :with => user.username
+  fill_in 'username', :with => user.username
   fill_in 'password', :with => user.password
   click_button "Log In"
 end
 
 def wrong_sign_in(user)
   visit "/login"
-  fill_in 'name', :with => user.username
+  fill_in 'username', :with => user.username
   fill_in 'password', :with => 'wrong password'
   click_button "Log In"
+end
+
+def sign_up(new_user)
+  visit "/users/new"
+  fill_in 'user_username', :with => new_user.username
+  fill_in 'user_password', :with => new_user.password
+  fill_in 'user_password_confirmation', :with => new_user.password_confirmation
+  click_button "Sign Up"
+  expect(page).to have_content "Thank you for signing up!"
 end
